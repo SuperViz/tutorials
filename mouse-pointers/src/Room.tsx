64e752decdf8a54b457a419f1e07cd9868b12d10
Carlos <carlos.santos74@outlook.com>
2024-08-06 08:51:51 -0300

    fixing

--- mouse-pointers/src/Room.tsx
+++ mouse-pointers/src/Room.tsx
@@ -1,30 +1,16 @@
-import { useCanvasPin, useComments, WhoIsOnline, Comments, MousePointers  } from "@superviz/react-sdk";
+import { MousePointers  } from "@superviz/react-sdk";
 
 export default function Room() {
-
-  const { openThreads, closeThreads } = useComments()
-  const { pin } = useCanvasPin({ canvasId: 'canvas' })
-
   return (
     <div className='w-full h-full bg-gray-200 flex items-center justify-center flex-col'>
       <header className='w-full p-5 bg-purple-400 flex items-center justify-between'>
         <h1 className='text-white text-2xl font-bold'>SuperViz Real-Time Mouse Pointers</h1>
-        <div id="comments" className='flex gap-2'></div>
       </header>
       <main className='flex-1 w-full h-full'>
         <div className='w-full h-full'>
           <canvas id="canvas" className='w-full h-full'></canvas>
         </div>
 
-        {/* SuperViz */}
-        <WhoIsOnline position='comments' />
-        <Comments 
-          pin={pin} 
-          position='left' 
-          buttonLocation='comments' 
-          onPinActive={openThreads}
-          onPinInactive={closeThreads}
-        />
         <MousePointers elementId="canvas" />
       </main>
     </div>
