0456411b641ec3129d58e781092092ae5543ae69
tommy-superviz <tommy@superviz.com>
2025-02-25 16:35:01 +0100

    Update huddle to 2.0

--- video-huddle/src/App.tsx
+++ video-huddle/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
 import { createRoom } from "@superviz/room";
-import { VideoHuddle } from "@superviz/video";
+import { MeetingState, VideoHuddle, VideoEvent } from "@superviz/video";
+
+import { useState } from "react";
 import { ImSpinner2 } from "react-icons/im";
 
+// SuperViz developer token ::
 const DEVELOPER_TOKEN = import.meta.env.VITE_SUPERVIZ_API_KEY;
 
 const App = () => {
+  // States ::
   const [isLoading, setIsLoading] = useState(false);
   const [meetingEnded, setMeetingEnded] = useState(false);
+  const [meetingStarted, setMeetingStarted] = useState(false);
 
+  // Initialize ::
   const initialize = async () => {
 
     setIsLoading(true);
@@ -25,31 +30,33 @@ const App = () => {
           id: "GROUP_ID",
           name: "GROUP_NAME",
         },
-        environment: "dev",
       });
 
-      const huddle = new VideoHuddle({
+      const video = new VideoHuddle({
         participantType: "host",
       });
 
-      huddle.subscribe("meeting.state.update", onMeetingStateUpdate);
-      huddle.subscribe("participant.left", onParticipantLeft);
+      video.subscribe(VideoEvent.MEETING_STATE_UPDATE, onMeetingStateUpdate);
+      video.subscribe(VideoEvent.MY_PARTICIPANT_JOINED, onMeetingStarted);
+      video.subscribe(VideoEvent.MY_PARTICIPANT_LEFT, onParticipantLeft);
 
-      room.addComponent(huddle);
+      room.addComponent(video);
     } catch (error) {
       console.error("Error initializing SuperViz Room:", error);
     }
   };
 
-  const onMeetingStateUpdate = (meetingState: any) => {
-
+  const onMeetingStateUpdate = (meetingState: MeetingState) => {
     // settings mounted remove loading ::
-    if (meetingState === 2) setIsLoading(false);
-
+    if (meetingState === MeetingState.MEETING_READY_TO_JOIN) setIsLoading(false);
   };
 
-  const onParticipantLeft = (participant: any) => {
+  const onParticipantLeft = () => {
     setMeetingEnded(true);
+  };
+
+  const onMeetingStarted = () => {
+    setMeetingStarted(true);
   };
 
   return (
@@ -58,12 +65,14 @@ const App = () => {
           <ImSpinner2 className="text-4xl text-white animate-spin" />
       ) : meetingEnded ? (
         <div className="text-lg font-semibold text-white">Thank you for joining the video huddle</div>
-      ) : (
+      ) : meetingStarted ? (
+        <div className="text-lg font-semibold text-white">Content going here</div>
+      ): (
         <button
           className="bg-[#6210cc] text-white px-5 py-3 text-xs rounded-lg"
           onClick={initialize}
         >
-          START VIDEO HUDDLE
+          START VIDEO CONFERENCE
         </button>
       )}
     </div>
