8878604a79c6923ef1097ab6bf8329814a5fd40a
tommy-superviz <tommy@superviz.com>
2025-02-27 11:45:39 +0100

    Update to v. 2.0

--- matterport-sales-tool/src/App.tsx
+++ matterport-sales-tool/src/App.tsx
@@ -1,15 +1,16 @@
-import { v4 as generateId } from 'uuid'
-import { useCallback, useEffect, useRef } from "react"
-import SuperVizRoom, { VideoConference } from '@superviz/sdk'
-import { Presence3D } from '@superviz/matterport-plugin'
+import { Presence3D } from '@superviz/matterport-plugin';
+import { VideoHuddle } from "@superviz/video";
+import { useCallback, useEffect, useRef } from "react";
+import { v4 as generateId } from 'uuid';
 // @ts-expect-error TODO: fix types
-import type { MpSdk } from '@superviz/matterport-plugin/dist/common/types/matterport.types.d.ts'
+import type { MpSdk } from '@superviz/matterport-plugin/dist/common/types/matterport.types.d.ts';
+import { createRoom } from '@superviz/room';
 
-const apiKey = import.meta.env.VITE_SUPERVIZ_API_KEY as string
-const matterportKey = import.meta.env.VITE_MATTERPORT_KEY as string
+// SuperViz developer token ::
+const DEVELOPER_TOKEN = import.meta.env.VITE_SUPERVIZ_API_KEY;
+const MATTERPORT_KEY = import.meta.env.VITE_MATTERPORT_KEY as string
 
 const ROOM_ID = 'matterport-sales-tool'
-const PLAYER_ID = generateId()
 
 interface MatterportIframe extends HTMLIFrameElement {
   window: Window & { MP_SDK: { connect: (window: Window, matterportKey: string) => Promise<MpSdk> } }
@@ -22,41 +23,43 @@ export default function App() {
     initializeMatterport()
   }, [])
 
-  const initializeSuperViz = useCallback(async () => { 
-    const superviz = await SuperVizRoom(apiKey, {
-      roomId: ROOM_ID,
-      participant: { 
-        id: PLAYER_ID,
-        name: 'player-name',
-      },
-      group: { 
-        id: 'matterport-sales-tool',
-        name: 'matterport-sales-tool',
-      }
-    })
+  const initializeSuperViz = useCallback(async () => {
+    try {
+      const room = await createRoom({
+        developerToken: DEVELOPER_TOKEN,
+        roomId: ROOM_ID,
+        participant: {
+          id: generateId(),
+          name: " ",
+        },
+        group: {
+          id: "GROUP_ID",
+          name: "GROUP_NAME",
+        },
+      });
 
-    const presence = new Presence3D(matterportSDK.current!)
-    superviz.addComponent(presence)
+      const presence = new Presence3D(matterportSDK.current!)
+      room.addComponent(presence)
 
-    const video = new VideoConference({
-      enableFollow: true,
-      enableGather: true,
-      enableGoTo: true,
-      userType: 'host'
-    })
+      const video = new VideoHuddle({
+        participantType: "host",
+      });
 
-    superviz.addComponent(video)
+     room.addComponent(video);
+    } catch (error) {
+      console.error("Error initializing SuperViz Room:", error);
+    }
   }, [])
 
   const initializeMatterport = useCallback(async () => {
     const showcase = document.getElementById('showcase') as MatterportIframe
     const showcaseWindow = showcase.contentWindow as MatterportIframe['window']
-    const source = `/vendor/matterport/showcase.html?&play=1&qs=1&applicationKey=${matterportKey}&m=Zh14WDtkjdC`;
+    const source = `/vendor/matterport/showcase.html?&play=1&qs=1&applicationKey=${MATTERPORT_KEY}&m=5m4i274y1aV`;
     showcase.setAttribute('src', source);
 
     await new Promise((resolve) => {
       showcase.addEventListener('load', async () => {
-        matterportSDK.current = await showcaseWindow?.MP_SDK.connect(showcaseWindow, matterportKey);
+        matterportSDK.current = await showcaseWindow?.MP_SDK.connect(showcaseWindow, MATTERPORT_KEY);
         resolve(matterportSDK.current);
       });
     });
@@ -68,9 +71,6 @@ export default function App() {
 
   return (
     <div className='w-full h-full bg-gray-200 flex items-center justify-center flex-col'>
-      <header className='w-full p-5 bg-purple-400 flex items-center justify-between'>
-        <h1 className='text-white text-2xl font-bold'>SuperViz Matterport Sales Tool</h1>
-      </header>
       <main className='w-full h-full flex items-center justify-center relative'>
         <iframe id='showcase' className='w-full h-full' />
       </main>
