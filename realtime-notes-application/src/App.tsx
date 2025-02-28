c2916e76ec4d92b88106b191344c14694faaec77
Carlos <carlos.santos74@outlook.com>
2025-02-28 12:11:16 -0300

    new realtime with collaboration

--- realtime-notes-application/src/App.tsx
+++ realtime-notes-application/src/App.tsx
@@ -1,98 +1,102 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import SuperVizRoom, {
-  LauncherFacade,
+import { createRoom } from "@superviz/room";
+import {
   MousePointers,
   WhoIsOnline,
-} from "@superviz/sdk";
+} from "@superviz/collaboration";
 import { Realtime, type Channel } from "@superviz/realtime/client";
 import { v4 as generateId } from "uuid";
 import { NoteNode } from "./components/note-node";
 import { Note } from "./common/types";
 
-const apiKey = import.meta.env.VITE_SUPERVIZ_API_KEY as string;
+// SuperViz developer token ::
+const DEVELOPER_TOKEN = import.meta.env.VITE_SUPERVIZ_API_KEY;
+
 const ROOM_ID = "video-huddle-application";
 const PARTICIPANT_ID = generateId();
 
 export default function App() {
   const [initialized, setInitialized] = useState(false);
   const [notes, setNotes] = useState<Note[]>([]);
-  const superviz = useRef<LauncherFacade | null>(null);
   const channel = useRef<Channel | null>(null);
 
   const initialize = useCallback(async () => {
     if (initialized) return;
 
-    const url = new URL(window.location.href);
-    const name = url.searchParams.get("name") || "Anonymous";
+    try {
+      const room = await createRoom({
+        developerToken: DEVELOPER_TOKEN,
+        roomId: ROOM_ID,
+        participant: {
+          id: PARTICIPANT_ID,
+          name: "Participant",
+        },
+        group: {
+          id: "GROUP_ID",
+          name: "GROUP_NAME",
+        },
+      });
 
-    superviz.current = await SuperVizRoom(apiKey, {
-      roomId: ROOM_ID,
-      participant: {
-        id: PARTICIPANT_ID,
-        name,
-      },
-      group: {
-        id: "realtime-sync",
-        name: "realtime-sync",
-      },
-    });
 
-    const realtime = new Realtime(apiKey, {
-      participant: {
-        id: PARTICIPANT_ID,
-        name,
-      },
-    });
+      const mousePointers = new MousePointers("mouse-container");
+      const whoIsOnline = new WhoIsOnline();
+      room.addComponent(mousePointers);
+      room.addComponent(whoIsOnline);
 
-    channel.current = await realtime.connect("realtime-sync");
+      const realtime = new Realtime(DEVELOPER_TOKEN, {
+        participant: {
+          id: PARTICIPANT_ID,
+        },
+      });
 
-    channel.current.subscribe<Note>("note-change", (event) => {
-      const note = event.data;
+      channel.current = await realtime.connect("realtime-sync");
+      channel.current.subscribe<Note>("note-change", (event) => {
+        const note = event.data;
 
-      if (event.participantId === PARTICIPANT_ID || !note) return;
+        if (event.participantId === PARTICIPANT_ID || !note) return;
 
-      setNotes((previous) => {
-        return previous.map((n) => {
-          if (n.id === note.id) {
-            return note;
-          }
+        setNotes((previous) => {
+          return previous.map((n) => {
+            if (n.id === note.id) {
+              return note;
+            }
 
-          return n;
+            return n;
+          });
         });
       });
-    });
 
-    const mousePointers = new MousePointers("mouse-container");
-    const whoIsOnline = new WhoIsOnline();
-    superviz.current.addComponent(mousePointers);
-    superviz.current.addComponent(realtime);
-    superviz.current.addComponent(whoIsOnline);
+      setInitialized(true);
+      setNotes([
+        {
+          id: "note-1",
+          title: `Unicorn's Shopping List`,
+          content: "Rainbow sprinkles, cloud fluff, and pixie dust",
+          x: 20,
+          y: 50,
+        },
+        {
+          id: "note-2",
+          title: `Zombie's To-Do List`,
+          content: "Find brains, practice groaning, shuffle aimlessly",
+          x: 20,
+          y: 50,
+        },
+        {
+          id: "note-3",
+          title: `Alien's Earth Observations`,
+          content:
+            "Humans obsessed with cat videos and avocado toast. Fascinating!",
+          x: 20,
+          y: 50,
+        },
+      ]);
+    } catch (error) {
+      console.error("Error initializing SuperViz Room:", error);
+    }
 
-    setInitialized(true);
-    setNotes([
-      {
-        id: "note-1",
-        title: `Unicorn's Shopping List`,
-        content: "Rainbow sprinkles, cloud fluff, and pixie dust",
-        x: 20,
-        y: 50,
-      },
-      {
-        id: "note-2",
-        title: `Zombie's To-Do List`,
-        content: "Find brains, practice groaning, shuffle aimlessly",
-        x: 20,
-        y: 50,
-      },
-      {
-        id: "note-3",
-        title: `Alien's Earth Observations`,
-        content:
-          "Humans obsessed with cat videos and avocado toast. Fascinating!",
-        x: 20,
-        y: 50,
-      },
-    ]);
+
+
   }, [initialized, setNotes]);
 
   const handleNoteChange = useCallback((note: Note) => {
